e131a2f2620c92ec76805ceb08d066ce9e632b56
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-05 10:29:50 +0530

    import java.util.Scanner; public class mul{     public static void main(String args[])     {         Scanner v = new Scanner(System.in);         int a;         double b;         a=v.nextInt();         b=3.14*a*a;         System.out.printf("%.2f",b);
        }
    }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class mul{
+    public static void main(String args[])
+    {
+        Scanner v = new Scanner(System.in);
+        int a;
+        double b;
+        a=v.nextInt();
+        b=3.14*a*a;
+        System.out.printf("%.2f",b);
+        
+    }
+}
