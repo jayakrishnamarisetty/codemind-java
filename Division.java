4257e7616b3d4dbd362970bf2a41ec1a900dbfb6
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-05 10:10:17 +0530

    import java.util.Scanner; public class avg{     public static void main(String args[])     {         Scanner v = new Scanner(System.in);         int a,b,c;         a=v.nextInt();         b=v.nextInt();         c=a/b;         System.out.println(c);
        }
    }

---
+++ Division.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class avg{
+    public static void main(String args[])
+    {
+        Scanner v = new Scanner(System.in);
+        int a,b,c;
+        a=v.nextInt();
+        b=v.nextInt();
+        c=a/b;
+        System.out.println(c);
+        
+    }
+}
