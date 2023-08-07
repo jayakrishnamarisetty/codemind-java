a555158d22ae9cd63f3aedb55c84a83d1e98221e
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-07 11:42:31 +0530

    import java.util.*; public class avg{     public static void main(String args[])     {         Scanner vyshu = new Scanner(System.in);         int a,b,c,d;         a=vyshu.nextInt();         b=vyshu.nextInt();         c=vyshu.nextInt();         d=3*a-b-c;         System.out.printf("%d",d);     } }

---
+++ Average_weight.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class avg{
+    public static void main(String args[])
+    {
+        Scanner vyshu = new Scanner(System.in);
+        int a,b,c,d;
+        a=vyshu.nextInt();
+        b=vyshu.nextInt();
+        c=vyshu.nextInt();
+        d=3*a-b-c;
+        System.out.printf("%d",d);   
+    }
+}
