658747dd319a3aceaf141fdae3b0b859a0585d45
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-17 11:46:48 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r = new Scanner(System.in);         int a=r.nextInt();         int b=r.nextInt();         if(a>=b)         {             System.out.println(a);         }         else         {             System.out.println(b);         }     } }

---
+++ Biggest_of_Two_Numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r = new Scanner(System.in);
+        int a=r.nextInt();
+        int b=r.nextInt();
+        if(a>=b)
+        {
+            System.out.println(a);
+        }
+        else
+        {
+            System.out.println(b);
+        }
+    }
+}
