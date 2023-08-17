e72a3ee9c9906811a6433f6eb5d32dc76c6b3679
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-17 11:53:25 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r = new Scanner(System.in);         int a=r.nextInt();         if(a>20)         {             System.out.println("HOT");         }         else{             System.out.println("COLD");         }     } }

---
+++ Climate.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r = new Scanner(System.in);
+        int a=r.nextInt();
+        if(a>20)
+        {
+            System.out.println("HOT");
+        }
+        else{
+            System.out.println("COLD");
+        }
+    }
+}
