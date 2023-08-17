453048f78029f17424903431ddd8512f80c5aee4
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-17 21:36:29 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r = new Scanner(System.in);         int a=r.nextInt();         if(a%2==0)         {             System.out.println("Even");         }         else{             System.out.println("Odd");         }     } }

---
+++ Check_if_number_is_even_or_odd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r = new Scanner(System.in);
+        int a=r.nextInt();
+        if(a%2==0)
+        {
+            System.out.println("Even");
+        }
+        else{
+            System.out.println("Odd");
+        }
+    }
+}
