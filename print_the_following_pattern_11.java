907e9074119c920acd9d14cd7cd8ce8167d6a267
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-19 10:49:21 +0530

    import java.util.*; public class pat {
            public static void main(String args[])
            {
                Scanner r = new Scanner(System.in);
                int n=r.nextInt();
                for(int i=1;i<=n;i++)
                {
                    for(int j=1;j<=n;j++)
                    {
                        System.out.print((char)(i+64)+" ");       
                    }
                    System.out.println( );
                }
            }
        
        
    }

---
+++ print_the_following_pattern_11.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class pat {
+
+        public static void main(String args[])
+        {
+            Scanner r = new Scanner(System.in);
+            int n=r.nextInt();
+            for(int i=1;i<=n;i++)
+            {
+                for(int j=1;j<=n;j++)
+                {
+                    System.out.print((char)(i+64)+" ");       
+                }
+                System.out.println( );
+            }
+        }
+    
+    
+}
