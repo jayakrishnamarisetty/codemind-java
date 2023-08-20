1e24805ef21530bf27135c08f8f7b18d1171b96a
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-20 11:43:37 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r = new Scanner(System.in);         double n=r.nextInt();         double c=32+(n*9/5);         System.out.printf("%.2f",c);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r = new Scanner(System.in);
+        double n=r.nextInt();
+        double c=32+(n*9/5);
+        System.out.printf("%.2f",c);
+    }
+}
