8792475bb7e3220509d816eba221a549e36048ec
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-25 10:41:10 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r=new Scanner(System.in);         String n=r.next();         System.out.println(Integer.parseInt(n,8));     } }

---
+++ Octal_to_Decimal_Conversion.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r=new Scanner(System.in);
+        String n=r.next();
+        System.out.println(Integer.parseInt(n,8));
+    }
+}
