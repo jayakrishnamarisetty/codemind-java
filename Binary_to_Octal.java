25eb185914813f0f615db2afefd06a3f6a55cb41
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-08-25 10:23:00 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r=new Scanner(System.in);         int n=r.nextInt();         for(int i=0;i<n;i++)         {             String a=r.next();             int d=Integer.parseInt(a,2);             System.out.println(Integer.toString(d,8));         }     } }

---
+++ Binary_to_Octal.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r=new Scanner(System.in);
+        int n=r.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            String a=r.next();
+            int d=Integer.parseInt(a,2);
+            System.out.println(Integer.toString(d,8));
+        }
+    }
+}
