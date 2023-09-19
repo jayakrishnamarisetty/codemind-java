926411b390fc12493c9c07f3504afb10eb9fc1a2
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-09-19 13:36:33 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r= new Scanner(System.in);         int n=r.nextInt();         int sum=0;         for(int i=1;i<=n;i++)         {             sum+=i;         }         System.out.print(sum+" ");     } }

---
+++ Sum_of_first_N_natural_numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class rug{
+    public static void main(String args[])
+    {
+        Scanner r= new Scanner(System.in);
+        int n=r.nextInt();
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            sum+=i;
+        }
+        System.out.print(sum+" ");
+    }
+}
