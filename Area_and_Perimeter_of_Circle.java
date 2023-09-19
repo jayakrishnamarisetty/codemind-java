d0d866c84bbc1c4515a10dec321db2c8ac8200ea
jayakrishnamarisetty <119914396+jayakrishnamarisetty@users.noreply.github.com>
2023-09-19 13:28:26 +0530

    import java.util.*; public class rug{     public static void main(String args[])     {         Scanner r= new Scanner(System.in);         int a=r.nextInt();         double ar=3.14*a*a;         double p=2*3.14*a;         System.out.printf("%.2f %.2f",ar,p);     } }

--- Area_and_Perimeter_of_Circle.java
+++ Area_and_Perimeter_of_Circle.java
@@ -2,11 +2,11 @@ import java.util.*;
 public class rug{
     public static void main(String args[])
     {
-        Scanner r = new Scanner(System.in);
+        Scanner r= new Scanner(System.in);
         int a=r.nextInt();
-        double b=3.14*a*a;
-        double c=2*3.14*a;
+        double ar=3.14*a*a;
+        double p=2*3.14*a;
         System.out.printf("%.2f
-%.2f",b,c);
+%.2f",ar,p);
     }
 }
